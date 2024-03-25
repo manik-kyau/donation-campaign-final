e09672f49d951d3aed9a24953cf1b5ba5fd422ca
manik-kyau <you@example.commaniksarkar.kyau.cse>
2024-03-25 04:31:20 -0700

    dfbdjsfh

--- src/Components/CustomSpinner/CustomSpinner.jsx
+++ src/Components/CustomSpinner/CustomSpinner.jsx
@@ -1,7 +1,7 @@
 const CustomSpinner = () => {
     return (
         <div className="text-center">
-            <span className="loading loading-spinner w-96 h-96 text-warning"></span>
+            <span className="loading loading-spinner w-10 h-10 text-warning"></span>
         </div>
     );
 };
